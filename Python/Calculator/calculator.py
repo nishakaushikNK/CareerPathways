5b5ec6ea65ed76ba4ec7a91f8c6f289b4d3776d6
Isaac Lin <isaac.lin@nagarro.com>
2023-04-21 17:01:23 -0400

    initial push

--- Python/Calculator/calculator.py
+++ Python/Calculator/calculator.py
@@ -1,66 +1,59 @@
-import math
+import math as m
 
-def add(num1, num2):
-    return num1+num2
+def Addition(number1, number2):
+    return number1+number2
 
-def subtract(num1, num2):
-    return num1-num2
+def Subtract(number1, number2):
+    return number1-number2
 
-def multiply(num1, num2):
-    return num1*num2
+def Multiply(number1, number2):
+    return number1*number2
 
-def divide(num1, num2):
-    return num1/num2
+def Divide(number1, number2):
+    return number1/number2
 
-def square(num):
-    return num*num
+def Percentage(number1, number2):
+    return int((number1/number2) * 100)
 
-def cube(num):
-    return num**3
+def Power(number1, number2):
+    return int((m.pow(number1, number2)))
 
-def squareRoot(num):
-    return math.sqrt(num)
 
-def power(base, power):
-    return base**power
+check = 1
+while check == 1:
+    print("Welcome to world of calculator")
+    print("Please select an operation:")
+    print("")
+    print("1. Add") 
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Percentage")
+    print("6. Power")
+    print("7. Exit")
+    print("")
+    Selection = int(input("Select the Operator: "))
+    if Selection == 7: 
+        check == 0
+    else: 
+        number1 = int(input("Enter the first number: "))
+        number2 = int(input("Enter the second number: "))
+        print("")
+        if Selection==1:
+            print("The result of Addition is: ", Addition(number1, number2))
+        elif Selection==2:
+            print("The result of Subtraction is: ", Subtract(number1, number2))
+        elif Selection==3:
+            print("The result of Multiplication is: ", Multiply(number1, number2))
+        elif Selection==4:
+            print("The result of Division is: ", Divide(number1, number2))
+        elif Selection==5:
+            print("The percentage is:",Percentage(number1, number2),"%")
+        elif Selection==6:
+            print("The result is: ", Power(number1, number2))
 
-print("Welcome to world of calculator")
-print("Please select an operation: ")
-print("1. Add")
-print("2. Subtract ")
-print("3. Multiply ")
-print("4. Divide ")
-print("5. Square")
-print("6. Cube ")
-print("7. Square root ")
-print("8. Power ")
-print("0. Exit ")
-answer = float(input("Please select an operation: "))
 
-while answer!=0:
-    if answer==1 or answer==2 or answer==3 or answer==4:
-        num1 = float(input("Enter your first number: "))
-        num2 = float(input("Enter your second number: "))
-        if (answer==1):
-            print(num1, " plus ", num2, " equals ", add(num1,num2))
-        elif (answer==2):
-            print(num1, " minus ", num2, " equals ", subtract(num1,num2))
-        elif (answer==3):
-            print(num1, " times ", num2, " equals ", multiply(num1,num2))
-        else:
-            print(num1, " divided by ", num2, " equals ", divide(num1,num2))
-    elif answer==5 or answer==6 or answer==7:
-        perform = float(input("Enter your number: "))
-        if (answer==5):
-            print(perform, " squared is ", square(perform))
-        elif (answer==6):
-            print(perform, " cubed is ", cube(perform))
-        else:
-            print("The square root of ", perform, " is ", squareRoot(perform))
-    elif answer==8:
-        base = float(input("What is your base: "))
-        pow = float(input("Enter your power: "))
-        print(base, " to the power of ", pow, " is ", power(base,pow))
-    answer = float(input("Please select an operation: "))
 
-print("Ok, thanks for playing!")
+
+
+
