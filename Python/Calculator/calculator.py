680661701df9fd6023c863d3497789268fb42245
Aarao Alexandre <alexandrea8@nycstudents.net>
2023-04-21 16:34:02 -0400

    smallChange

--- Python/Calculator/calculator.py
+++ Python/Calculator/calculator.py
@@ -1,5 +1,29 @@
 import math
 
+def add(num1, num2):
+    return num1+num2
+
+def subtract(num1, num2):
+    return num1-num2
+
+def multiply(num1, num2):
+    return num1*num2
+
+def divide(num1, num2):
+    return num1/num2
+
+def square(num):
+    return num*num
+
+def cube(num):
+    return num**3
+
+def squareRoot(num):
+    return math.sqrt(num)
+
+def power(base, power):
+    return base**power
+
 print("Welcome to world of calculator")
 print("Please select an operation: ")
 print("1. Add")
@@ -18,25 +42,25 @@ while answer!=0:
         num1 = float(input("Enter your first number: "))
         num2 = float(input("Enter your second number: "))
         if (answer==1):
-            print(num1, " plus ", num2, " equals ", (num1+num2))
+            print(num1, " plus ", num2, " equals ", add(num1,num2))
         elif (answer==2):
-            print(num1, " minus ", num2, " equals ", (num1-num2))
+            print(num1, " minus ", num2, " equals ", subtract(num1,num2))
         elif (answer==3):
-            print(num1, " times ", num2, " equals ", (num1*num2))
+            print(num1, " times ", num2, " equals ", multiply(num1,num2))
         else:
-            print(num1, " divided by ", num2, " equals ", (num1/num2))
+            print(num1, " divided by ", num2, " equals ", divide(num1,num2))
     elif answer==5 or answer==6 or answer==7:
         perform = float(input("Enter your number: "))
         if (answer==5):
-            print(perform, " squared is ", (perform*perform))
+            print(perform, " squared is ", square(perform))
         elif (answer==6):
-            print(perform, " cubed is ", (perform**3))
+            print(perform, " cubed is ", cube(perform))
         else:
-            print("The square root of ", perform, " is ", math.sqrt(perform))
+            print("The square root of ", perform, " is ", squareRoot(perform))
     elif answer==8:
         base = float(input("What is your base: "))
-        power = float(input("Enter your power: "))
-        print(base, " to the power of ", power, " is ", (base**power))
+        pow = float(input("Enter your power: "))
+        print(base, " to the power of ", pow, " is ", power(base,pow))
     answer = float(input("Please select an operation: "))
 
 print("Ok, thanks for playing!")
