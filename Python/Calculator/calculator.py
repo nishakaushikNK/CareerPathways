7fba6f450f48b41800760839b2bfac7aafa590b6
Jason <jason.tenelema@nagarro.com>
2023-04-25 17:34:42 -0400

    calculator with functions

--- Python/Calculator/calculator.py
+++ Python/Calculator/calculator.py
@@ -1,26 +1,37 @@
+#math library
 import math as m
-print("Welcome to world of calculator")
-print("JTCaclculator\n1:Add\n2:subtract\n3:multiply\n4:Divide\n5:Root\n6:Power")
 
-a=int(input("First Number"))
-op = input("Which operation?")
-b=int(input("Second Number"))
+numbers={1:"one",2:"two"}
+def intro():
+    print("Welcome to world of calculator")
+    print("JTCaclculator\n1:Add\n2:subtract\n3:multiply\n4:Divide\n5:Root\n6:Power\n0:exit")
 
-if op=="3":
-    result=int(a)*int(b)
-    print(result)
-elif op=="1":
-    result=a+b
-    print(result)
-elif op=="2":
-    result=a-b
-    print(result)
-elif op=="4":
-    result=a/b
-    print(result)
-elif op=="5":
-    result=m.pow(a,(1/b))
-    print(result)
-elif op=="6":
-    result=m.pow(a,b)
-    print(result)
+def add(a,b):
+    return(a+b)
+def sub(a,b):
+    return(a-b)
+def mult(a,b):
+    return(a*b)
+def div(a,b):
+    return(a/b)
+def root(a,b):
+    return(m.pow(a,(1/b)))
+def pow(a,b):
+    return(m.pow(a,b))
+
+funcL=[add,sub,mult,div,root,pow]
+
+def logic(op,num1,num2):
+    return funcL[op-1](num1,num2)
+
+def main():
+    intro()
+    operation=int(input("choose an operation: "))
+    if operation==0:
+        print("Left JT Claculaotr")
+    else:
+        num1=int(input("FirstNumber: "))
+        num2=int(input("Second Number: "))
+        print(logic(operation,num1,num2))
+
+main()
