39eee68a52f839bf26971ecaf7a5647958f38e66
Nisha Kaushik <NKaushik@schools.nyc.gov>
2023-04-28 15:12:36 -0400

    api

--- Python/Flask/app/app.py
+++ Python/Flask/app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +7,53 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/getweatherdata')
+def getdate():
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "lat": 44.34,
+        "lon": 10.99,
+        "appid": "356ebba58f9edb6a663e344d7fd81b43"
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+        print("Weather data:", data)
+    else:
+        return "error"
+        print(f"Error {response.status_code}: {response.text}")
+
+def get_weather_data(lat, lon, appid):
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": appid
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+@app.route('/weather_data/<float:lat>/<float:lon>')
+def weather_data(lat, lon):
+    appid = "356ebba58f9edb6a663e344d7fd81b43"
+    data = get_weather_data(lat, lon, appid)
+
+    if data:
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Unable to fetch weather data."}), 500
+
+
+def hello_world():
+    return 'Hello World'
 
 # @app.route("/")
 # def index():
